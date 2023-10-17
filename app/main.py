daf03e447f981755add7846f796b7aa22350b28e
huanvt-nals <huanvt@nal.vn>
2023-10-17 22:03:53 +0700

    Fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from typing import Union
+# from typing import Union
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4
